8334b79aed6f4ce8398de717f50eaf8aa32809bb
SamuelPKM <samuel11299ac@gmail.com>
2021-10-20 13:13:19 -0500

    Agregar y mostrar nodo funcionando
    Hasta aqui tenemos lo necesario para agregar y mostrar nodos, nos falta el arreglo visual con bootstrap o alguna otra librería

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ function App() {
     const usersCollectionRef = collection(DB, "Usuarios")
 
     const createUser = async ()=>{
-
+      //Importante el nombre de los campos, deben mandarse tal cual escritos para poderlos leer en la parte de mostrar nodo
+      await addDoc(usersCollectionRef, {Nickname: newNname, Escuela: newEscuela, ID_Profesor: newProfesor, Unidad: newUDA, Grupo: newGrupo})
     }
     useEffect(() => {
 
@@ -41,11 +42,48 @@ function App() {
         <br></br>
         <small id="helpId" class="form-text text-muted">Aqui debes insertar tu nombre de usuario</small>
         <br></br>
+        <label for="">Escuela</label>
+        <input type="text"
+          class="form-control" name="Nickname" id="" aria-describedby="helpId" placeholder="Escuela " 
+          onChange={(event) => {setnewEscuela(event.target.value);
+          }}>
+        </input>
+        <br></br>
+        <small id="helpId" class="form-text text-muted">Aqui debes insertar el nombre de tu Escuela</small>
+        <br></br>
+        <label for="">Grupo</label>
+        <input type="text"
+          class="form-control" name="Nickname" id="" aria-describedby="helpId" placeholder="Grupo " 
+          onChange={(event) => {setnewGrupo(event.target.value);
+          }}>
+        </input>
+        <br></br>
+        <small id="helpId" class="form-text text-muted">Aqui debes insertar el nombre de tu Grupo</small>
+        <br></br>
+        <label for="">Profesor</label>
+        <input type="text"
+          class="form-control" name="Nickname" id="" aria-describedby="helpId" placeholder="Profesor " 
+          onChange={(event) => {setnewProfesor(event.target.value);
+          }}>
+        </input>
+        <br></br>
+        <small id="helpId" class="form-text text-muted">Aqui debes insertar tu identificador de Profesor</small>
+        <br></br>
+        <label for="">Unidad</label>
+        <input type="text"
+          class="form-control" name="Nickname" id="" aria-describedby="helpId" placeholder="Unidad " 
+          onChange={(event) => {setnewUDA(event.target.value);
+          }}>
+        </input>
+        <br></br>
+        <small id="helpId" class="form-text text-muted">Aqui debes insertar tu Unidad</small>
+        <br></br>
         <button type="submit" class="btn btn-primary" onClick={createUser}>Enviar</button>
       </div>
 
       {Usuarios.map((Usuarios) => {
         return <div>
+          <br></br>
           <h1>Usuario: {Usuarios.Nickname}</h1>
           <h1>Escuela: {Usuarios.Escuela}</h1>
           <h1>Grupo: {Usuarios.Grupo}</h1>
